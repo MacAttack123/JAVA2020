7d0576879fe7720017b4a10651d3fd63eb149882
MackAttack123 <tracys284@hotmail.com>
2020-05-02 11:15:11 +0100

    Beer Bottle Song

---
+++ BeerBottles/src/tracy/learningprogramming/Main.java
@@ -0,0 +1,39 @@
+package tracy.learningprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Loop practice with 99 Bottles of beer song
+
+        int beerNum = 99;
+        String word = "bottles";
+        String words = " of beer on the wall.";
+        String none = "No more bottles of beer on the wall.";
+
+        while (beerNum>0){
+
+            if (beerNum ==1){
+
+                word = "bottle";
+            }
+
+            System.out.println(beerNum + " " + word + words);
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum = beerNum -1;
+
+            if (beerNum > 0) {
+
+                System.out.println(beerNum + " " + word + words);
+            }
+                    else {
+                System.out.println(none);
+
+
+                }
+            }
+        }
+
+    }
+
